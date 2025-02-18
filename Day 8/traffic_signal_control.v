bd5b0845cdb675186219589233ee34d452c4145b
Abhishek <abhishek09241@gmail.com>
2025-02-18 22:28:58 +0530

    Traffic Signal Control

---
+++ Day 8/traffic_signal_control.v
@@ -0,0 +1,14 @@
+`define TRUE  1'b1
+`define FALSE 1'b0
+
+
+// Delays
+`define Y2RDELAY 3 // Yellow to Red delay
+`define R2YDELAY 2 // Red to Yellow delay
+
+
+module traffic_signal_control (
+    
+);
+    
+endmodule
